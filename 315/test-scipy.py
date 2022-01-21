808644c72ecb5ac1e3c4a717367b37cdf10c50ba
grahamwwilson <gwwilson@ku.edu>
2022-01-21 11:31:32 -0600

    Add version info

--- 315/test-scipy.py
+++ 315/test-scipy.py
@@ -1,4 +1,9 @@
+import scipy
 from scipy.special import comb
+
+#Check scipy version
+print('scipy version',scipy.__version__)
+
 #find combinations of 5, 2 values using comb(N, k)
-com = comb(5, 2, exact = False, repetition=True)
-print(com)
+combos = comb(5, 2, exact = False, repetition=True)
+print('number of combinations: ',int(combos))
