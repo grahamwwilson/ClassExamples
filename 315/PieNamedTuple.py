b83dbb78bcc04c4a4497e4e0b5f0b14d1d6ec186
grahamwwilson <gwwilson@ku.edu>
2022-01-26 19:04:01 -0600

    Add NamedTuple based implementation

---
+++ 315/PieNamedTuple.py
@@ -0,0 +1,68 @@
+# PieNamedTuple.py
+import random
+import math
+from typing import NamedTuple
+
+class Estimator(NamedTuple):
+    nsuccesses: int
+    estimate: float
+    error: float
+
+# Refactor as a function
+# Illustrate use of random numbers for integration
+
+def PiEstimate(SEED, N):
+# SEED = random number seed
+# N    = number of trials 
+
+# Initialize the random number generator using specified seed
+   random.seed(SEED)
+
+# Keep track of number within unit circle of radius, 1.0
+   ncircle = 0
+
+# Generate multiple sets of two uniform random numbers each 
+# in the range (-1.0,1.0). So random points inside a square with 
+# sides of length 2 centered on (0,0)
+# The square has an area of 2*2 = 4 units. 
+# What is the area of the circle with radius 1?
+
+   for i in range(N):
+       x = random.uniform(-1.0,1.0)
+       y = random.uniform(-1.0,1.0)
+# calculate the distance from the origin
+       r = math.sqrt(x*x + y*y)
+       if r < 1.0:
+           ncircle += 1   # point is inside the circle, so count it
+
+   print('Number of trial (x,y) values that are inside the circle of radius 1.0 = ',ncircle)
+
+#Add extra code here from circle.txt
+   fraction = float(ncircle)/float(N)
+   print('fraction ',fraction)
+   print('4*fraction ',4.0*fraction)
+
+   p = fraction
+   dNobs = math.sqrt(float(N)*p*(1.0-p))   # Binomial error on success number (ncircle)
+   print('Binomial uncertainty on nsuccesses: ',dNobs)
+   dp = dNobs/float(N)
+
+   mypi = 4.0*p
+   dmypi = 4.0*dp
+   print('Estimate of pi = ',mypi,' +- ',dmypi)
+   pcdeviation = 100.0*(mypi - math.pi)/math.pi
+   print('This deviates from ',math.pi,' by ',pcdeviation,'%')
+   print('The rel. statistical uncertainty is ',100.0*dmypi/mypi,'%')
+   ptrue = math.pi/4.0
+   dNtrue = math.sqrt(float(N)*ptrue*(1.0-ptrue))
+   dptrue = dNtrue/float(N)
+   dpitrue = 4.0*dptrue
+   dstat = 100.0*dpitrue/math.pi
+   print('Using correct p, stat. uncertainty on N',dNtrue,' rel. uncertainty ',dstat,'%')
+   
+   error = dpitrue
+
+# Here we use a dataclass to return the information
+   e = Estimator(ncircle,mypi,error)
+
+   return e
---
+++ 315/ntCircle.py
@@ -0,0 +1,27 @@
+# ntCircle.py
+import random
+import math
+from PieNamedTuple import PiEstimate
+import ROOT
+
+# Histogram initialization
+f = ROOT.TFile("histos_Pie.root", "recreate")
+h0 = ROOT.TH1D("h0","h0; Successes",200,264.25,364.25)
+#h0 = ROOT.TH1D("h0","h0; Successes",200,2640.5,3640.5)
+h1 = ROOT.TH1D("h1","h1; Pi Estimate",400,2.6,3.6)
+h2 = ROOT.TH1D("h2","h2; Normalized Deviation",200,-5.0,5.0)
+
+# 10,000 repetitions of the experiment with different seeds
+N = 400
+for i in range(10000):
+    SEED = 200 + i
+    e = PiEstimate(SEED, N)
+    h0.Fill(e.nsuccesses)
+    h1.Fill(e.estimate)
+    ndev = (e.estimate - math.pi)/e.error
+    h2.Fill(ndev)
+
+h0.Draw()
+h1.Draw()
+h2.Draw()
+f.Write()
