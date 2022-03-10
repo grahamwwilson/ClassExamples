ba30a78c703f3d6aced825c2181b370a1ffd8482
grahamwwilson <gwwilson@ku.edu>
2022-03-10 10:32:48 -0600

    Simplify further

--- ODEs/pendulum.py
+++ ODEs/pendulum.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import myPythonCheck
 import pendArgs
-import stepper                  # ODE stepping algorithm
+from stepper import Stepper     # ODE stepping algorithm
 import pendulumODE as p         # ODE specific functions
 #
 # Solve the initial-value problem ODE for a simple pendulum
@@ -16,22 +16,20 @@ ofile = open(ofname, "w")
 y0 = np.array( [ theta, omega ])
 y  = np.copy(y0)   # Set initial values
 
-t = 0.0
 parameters = [g, l]
 
 E0 = p.Energy(y, parameters)       # Initial energy - hopefully this is conserved!
 istep = 0
 t = 0.0
-yprev = y
 
 # Write header and initial conditions to output file
 p.PrintHeader(ofile)
 p.PrintStep(y, parameters, t, istep, E0, ofile)
 
 while t < tMax:
-    yprev = y; tprev = t
-# Calculate new position and velocity of projectile using chosen method
-    y, t, istep = stepper.Stepper(y, dt, parameters, istep, sMethod)
+# Calculate one time-step advanced theta and angular velocity 
+# of pendulum using chosen method
+    y, t, istep = Stepper(y, dt, parameters, istep, sMethod)
     p.PrintStep(y, parameters, t, istep, E0, ofile)    
 
 p.PrintState(y, parameters, t, istep, E0)
