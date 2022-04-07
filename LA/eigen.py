d3f5d137e69558259a28520d97006707228e4b6c
grahamwwilson <gwwilson@ku.edu>
2022-04-07 13:05:03 -0500

    Basic linear algebra examples

---
+++ LA/eigen.py
@@ -0,0 +1,61 @@
+import numpy as np
+
+#A = np.mat("1 2 3; 1 3 4; 3 2 1")
+
+A = np.mat("1 2; 1 3")
+  
+# Original matrix
+print("A",A.shape)
+print(A)
+print("")
+evalue, evect = np.linalg.eig(A)
+  
+# Eigenvalues of the matrix"
+print("evalue",evalue.shape)
+print(evalue)
+lambda0 = evalue[0]
+lambda1 = evalue[1]
+print("Eigenvalues:",lambda0,lambda1)
+print("")
+  
+# Eigenvectors of the matrix
+print("evect",evect.shape)
+print(evect)
+
+# Check these make sense as the right eigenvectors for eigenvalues lambda0, lambda1
+vr0 = evect[:,0]
+vr1 = evect[:,1]
+
+print("vr0",vr0.shape)
+print(vr0)
+print()
+
+print("vr1",vr1.shape)
+print(vr1)
+print()
+
+# Test A vri  = lambdai v for i=0,1
+#
+#
+lhs0 = np.dot(A,vr0)
+rhs0 = np.dot(lambda0,vr0)
+print("lhs0:")
+print(lhs0)
+print("rhs0:")
+print(rhs0)
+print("lhs0-rhs0:")
+print(lhs0-rhs0)
+
+print()
+lhs1 = np.dot(A,vr1)
+rhs1 = np.dot(lambda1,vr1)
+print("lhs1:")
+print(lhs1)
+print("rhs1:")
+print(rhs1)
+print("lhs1-rhs1:")
+print(lhs1-rhs1)
+
+
+
+ 
---
+++ LA/solve.py
@@ -0,0 +1,31 @@
+import numpy as np
+from scipy import linalg
+
+A = np.array([[3, 2, 0], [1, -1, 0], [0, 5, 1]])  # 3*3 array of 3 row vectors
+b = np.array([[2], [4], [-1]])                    # column vector b
+
+x = linalg.solve(A, b)                            # solution column vector x
+
+print()
+print("Check scipy.linalg.solve for solving a set of linear equations")
+print()
+
+print("A:", A.shape)
+print(A)
+print()
+print("b:", b.shape)
+print(b)
+print()
+
+print("Solution vector x:",x.shape)
+print(x)
+print()
+
+# Double-check that x is indeed a solution of A x = b
+c = np.dot(A,x)
+print("c:",c.shape)
+print(c)
+print()
+diff = c - b
+print("Difference vector:",diff.shape)
+print(diff)
