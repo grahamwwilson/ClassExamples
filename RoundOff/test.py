671927b0302904f1a6f31d352f4bb6faf2e8455a
grahamwwilson <gwwilson@ku.edu>
2023-04-12 12:06:58 -0500

    Update

--- RoundOff/test.py
+++ RoundOff/test.py
@@ -1,9 +1,9 @@
 #
 # Demonstrate round-off issues with 64-bit floating point numbers
 #
-h=1.0
-one=1.0
-for i in range(100):
+h = 1.0
+one = 1.0
+for i in range(65):
     x = one + h
     print('Loop index',i,' h:',h,' x=1+h:',x,' x-1: ',x-one)
     h = h/2.0   
