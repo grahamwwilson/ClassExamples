7ad7f982f1789f6248f1b3dd39fcdbeebdf278e0
grahamwwilson <gwwilson@ku.edu>
2023-10-26 09:56:56 -0500

    Add normalized deviation calculation

--- PValues/pvalue.py
+++ PValues/pvalue.py
@@ -2,6 +2,7 @@
 
 import myPythonCheck
 import pvalueArgs
+import numpy as np
 from scipy import stats                       
 
 myPythonCheck.Check()                         # Enforce use of python3
@@ -16,6 +17,12 @@ pvalueArgs.showArgs(chsq, ndof)
 pvaluepercent = 100.0*(1.0 - stats.chi2.cdf(chsq, ndof ))
 print(' ')
 print('Observed chi-squared p-value of',pvaluepercent,'%')
+
+# Also calculate standardized z-score
+# note that this retains the intrinsic asymmetry of the chi-squared distribution
+# and is NOT the Gaussian equivalent z-score from the p-value.
+z = (chsq - ndof)/np.sqrt(2.0*ndof)
+print(' Normalized deviation = ',z)
 
 # A p-value exceeding 5% say should happen 95% of the time 
 # when the model is correct. One therefore needs to have 
